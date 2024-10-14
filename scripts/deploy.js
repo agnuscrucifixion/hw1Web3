5e19ab5463593f1fda99d0b8ccf51c043184e482
agnuscrucifixion <nikitabysko123@gmail.com>
2024-10-14 19:41:15 +0300

    edit deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,20 +1,22 @@
-const { ethers } = require("hardhat");
-
 async function main() {
     const [deployer] = await ethers.getSigners();
 
-    const ERC20Token = await ethers.getContractFactory("MyToken");
-    const erc20 = await ERC20Token.deploy(1000000);
-    console.log("ERC20 deployed to:", erc20.address);
+    console.log("Deploying contracts with the account:", deployer.address);
 
-    const ERC721Token = await ethers.getContractFactory("MyNFT");
-    const erc721 = await ERC721Token.deploy();
-    console.log("ERC721 deployed to:", erc721.address);
+    const ERC20 = await ethers.getContractFactory("MyERC20Token");
+    const erc20 = await ERC20.deploy();
+    await erc20.waitForDeployment(); // Ждем завершения деплоя
+    console.log("ERC20 contract deployed to:", await erc20.getAddress());
 
-    // Деплой ERC1155
-    const ERC1155Token = await ethers.getContractFactory("MyMultiToken");
-    const erc1155 = await ERC1155Token.deploy();
-    console.log("ERC1155 deployed to:", erc1155.address);
+    const ERC721 = await ethers.getContractFactory("MyERC721Token");
+    const erc721 = await ERC721.deploy();
+    await erc721.waitForDeployment();
+    console.log("ERC721 contract deployed to:", await erc721.getAddress());
+
+    const ERC1155 = await ethers.getContractFactory("MyERC1155Token");
+    const erc1155 = await ERC1155.deploy();
+    await erc1155.waitForDeployment(); // Ждем завершения деплоя
+    console.log("ERC1155 contract deployed to:", await erc1155.getAddress());
 }
 
 main().catch((error) => {
